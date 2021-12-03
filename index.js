e762e6b93ee8019e71c335b698cca37189d15ff5
Luka Ku <lukaszkuc93@gmail.com>
2021-12-03 16:46:58 +0100

    add adding new task

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const task = document.querySelector('.task')
 const allTask = document.querySelectorAll('.task')
 const addBtn = document.querySelector('.add-btn')
+const ulList = document.getElementById('ul-list')
 
-addBtn.addEventListener('click', ()=>{
-    console.log('dziala')
-})
+
+
+
+
+
+
+function addTask () {
+    const newTask = prompt('Wpisz zadanie')
+    const ulList = document.getElementById('ul-list')
+    const newLi = document.createElement('li')
+    const trash = document.createElement('button')
+    
+    newLi.classList.add('task')
+   
+    const inputElement = document.createElement('input')
+    inputElement.type = 'checkbox'
+    inputElement.id = "task-1"
+    newLi.appendChild(inputElement)
+
+    const newLabel = document.createElement('label')
+    newLabel.setAttribute('for', )
+    newLabel.innerText = newTask
+    newLi.appendChild(newLabel)
+    
+    trash.setAttribute("id","trash")
+
+    newLi.appendChild(trash)
+
+
+    ulList.appendChild(newLi)
+    
+}
+
+addBtn.addEventListener('click', addTask)
 
 allTask.forEach((task)=>{
-    const trash = task.querySelector('.trash')
+    const trash = task.lastChild
 
     function removeTasks (){
         task.remove()
